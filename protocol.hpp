fa4707d313cc36fe482dc1efe1e608307e060bc1
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2019-07-14 15:18:10 +0200

    added: initial development draft.

---
+++ protocol.hpp
@@ -0,0 +1,81 @@
+#pragma once
+
+// development draft
+
+namespace ledframe {
+namespace proto {
+
+enum class command : u8 {
+	undef = 0x00,
+	init,
+	sync,
+	info,
+	push,
+	clear,
+	done,
+};
+
+enum class response {
+	none,
+	ready,
+	synced,
+};
+
+struct command_query {
+	constexpr command_query(const command cmd) : cmd(static_cast<u8>(cmd)) {
+	}
+
+	const char header[3]{'L', 'F', '_'};
+	const u8 cmd;
+	const char end_header = '\0';
+};
+
+struct command_push_params {
+	u16 led_count;
+	u16 sum;
+};
+
+struct command_query_push {
+	command_query header{command::push};
+	command_push_params params{};
+};
+
+
+enum class position : u8 {
+	left,
+	top,
+	right,
+	bottom,
+	center
+};
+
+enum class order : u8 {
+	clockwise,
+	counter_clockwise,
+};
+
+struct strip_param {
+	u8 palette : 4;
+	u8 ord : 1;
+	u8 pos : 3;
+	u16 count;
+};
+
+static_assert(sizeof(strip_param) == 3);
+
+struct command_info_params {
+	u8 version{};
+	u8 sequences{};
+	char name[32]{};
+	strip_param directions[16]{};
+};
+
+// static tests
+static_assert(sizeof(command_query(command::undef)) == 5);
+static_assert(sizeof(command_query_push) == 9);
+
+static_assert(sizeof(strip_param) == 3);
+static_assert(sizeof(command_info_params) == 82);
+
+}
+}
